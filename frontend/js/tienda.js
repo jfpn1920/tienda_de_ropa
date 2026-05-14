e3e5909d1ac53d74b094e3cd0a936789e05233d7
jfpn1920 <juanfelipepnieto1920@gmail.com>
2026-05-13 21:53:32 -0500

    agregando_adaptablidad_a_tienda

--- frontend/js/tienda.js
+++ frontend/js/tienda.js
@@ -4,7 +4,7 @@
 const menuToggle = document.getElementById("menuToggle");
 const sidebar = document.getElementById("sidebar");
 const contents = document.querySelectorAll(
-    ".main-content-8, .main-content-9, .main-content-10, .main-content-11, .main-content-12, .main-content-13"
+    ".main-content-8, .main-content-9, .main-content-10, .main-content-11, .main-content-12, .main-content-13, .main-content-14, .contenido-container5, .toolbar5"
 );
 menuToggle.addEventListener("click", () => {
     sidebar.classList.toggle("active");
